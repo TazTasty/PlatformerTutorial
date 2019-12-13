2e6cdd0aebbbce5a7af6993f282520ee405d92ca
Luke Roby <20.luke.roby@ksd.org>
2019-12-13 10:12:31 -0800

    platformers

---
+++ PlatformyThingsTutorial/Assets/Scripts/DamageCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCalculator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SimpleFunction();
+        int dmg = DamageCalc(15, 3);
+
+    }
+    void SimpleFunction()
+    {
+        Debug.Log("Hello there!");
+    }
+
+    public int DamageCalc(int damage, int armor)
+    {
+        Debug.Log("You deal" + (damage - armor) + "damage");
+        return damage - armor;
+    }
+}
---
+++ PlatformyThingsTutorial/Assets/Scripts/Dialogue/TriggerDialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour
+{
+    public ObjectDialogue dialogue;
+
+    public void TriggerObjectDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
